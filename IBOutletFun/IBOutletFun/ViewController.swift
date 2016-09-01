215a59e2bae1515a1a5ae1e0f5b4c9ff416dbc5d
jacqschweiger <jacqschweiger@gmail.com>
2016-09-01 15:09:31 -0400

    Done

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(sender: AnyObject) {
+        print("Pressed Hello World button")
+        textDisplay.text = "Hello world!"
+    }
+
+    @IBAction func goodbyeWorld(sender: AnyObject) {
+        print("Pressed Goodbye World button")
+        textDisplay.text = "Goodbye world!"
+    }
+    
+    @IBAction func unicorn(sender: AnyObject) {
+        print("Pressed Unicorns! button")
+        textDisplay.text = "Unicorns!"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
         // Do any additional setup after loading the view, typically from a nib.
     }
 
